8c32eb32a6d9928e54c2caf5a692703ee30b94cc
Aiririri <amirbaiguzhinov2005@gmail.com>
2026-03-01 20:30:47 +0500

    Add files via upload

---
+++ Lab6/album-browser/src/app/components/album-detail/album-detail.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AlbumService } from '../../services/album.service';
+import { Album } from '../../models/album.model';
+
+@Component({
+  selector: 'app-album-detail',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './album-detail.component.html',
+  styleUrl: './album-detail.component.css'
+})
+export class AlbumDetailComponent implements OnInit {
+  album: Album | null = null;
+  loading = true;
+  saving = false;
+  savedMessage = false;
+  editTitle = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private albumService: AlbumService
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.albumService.getAlbum(id).subscribe({
+      next: (data) => {
+        this.album = data;
+        this.editTitle = data.title;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+
+  saveAlbum(): void {
+    if (!this.album) return;
+    this.saving = true;
+    const updated = { ...this.album, title: this.editTitle };
+    this.albumService.updateAlbum(updated).subscribe({
+      next: () => {
+        this.album!.title = this.editTitle;
+        this.saving = false;
+        this.savedMessage = true;
+        setTimeout(() => this.savedMessage = false, 2500);
+      },
+      error: () => {
+        this.saving = false;
+      }
+    });
+  }
+}
